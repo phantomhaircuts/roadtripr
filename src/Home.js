6c344123f915415d4006ec60b50c2b83e0971df5
Erik Anderson <itserikanderson@gmail.com>
2017-04-04 11:22:49 -0400

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import NavContainer from './NavContainer';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <NavContainer />
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/NavContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NavContainer extends Component {
+  render() {
+    return (
+      <div className="NavContainer">
+        <h1>Roadtripr</h1>
+      </div>
+    );
+  }
+}
+
+export default NavContainer;
