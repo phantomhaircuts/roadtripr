84f71936da664ed3ece67b2b61e28f255c264eac
Angel Valant <angelgithub@gmail.com>
2017-04-04 11:35:05 -0400

    adds basic seed file

---
+++ data.js
@@ -0,0 +1,7 @@
+const users = [
+  { firstName: "Erik", lastName: "Anderson", email: "test1@email.com", password: "test1" },
+  { firstName: "Angel", lastName: "Valant", email:"test2@email.com", passed: "test2" }
+]
+
+
+module.exports = data;
