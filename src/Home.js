d7f572f8f67aa3c715931f7278746d0f93b25236
Erik Anderson <itserikanderson@gmail.com>
2017-04-04 11:30:40 -0400

    added user component

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import NavContainer from './NavContainer';
+import User from './User'
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
         <NavContainer />
+        <User />
       </div>
     );
   }
